637efd7789a0d76bf15269159183142ef57c35ca
김성백 <dumbuk12@naver.com>
2021-12-12 17:13:02 +0900

    feat: RegisterForm에 react-hook-form 적용

--- src/features/auth/components/RegisterForm/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm/RegisterForm.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   FormControl,
   FormLabel,
+  FormErrorMessage,
   Input,
   Stack,
   Button,
@@ -10,52 +11,163 @@ import {
   Select,
   Link as ChakraLink,
 } from "@chakra-ui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
 import React from "react";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { Link } from "react-router-dom";
+import * as yup from "yup";
+
+import { DatePicker } from "@/components/DatePicker";
+
+const schema = yup.object().shape({
+  name: yup.string().min(2).max(255).required(),
+  gender: yup.string().oneOf(["man", "female"]).required(),
+  birthDate: yup.date().required(),
+  nickname: yup.string().min(2).max(255).required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(3).max(255).required(),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null])
+    .required(),
+});
+
+const defaultValues: FormValues = {
+  name: "",
+  gender: "man",
+  birthDate: new Date(),
+  nickname: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+type FormValues = {
+  name: string;
+  gender: string;
+  birthDate: Date;
+  nickname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 export const RegisterForm = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+    watch,
+    control,
+  } = useForm<FormValues>({
+    defaultValues,
+    resolver: yupResolver(schema),
+  });
+
+  const sleep = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    await sleep(1000);
+    alert(JSON.stringify(data));
+  };
+
+  // 현재 Form에 입력된 데이터를 조회하기 위한 코드
+  console.log(watch());
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Stack spacing={4}>
-        <FormControl id="name">
+        <FormControl id="name" isInvalid={Boolean(errors.name)}>
           <FormLabel>이름</FormLabel>
-          <Input type="text" placeholder="이름을 입력해주세요." />
+          <Input
+            type="text"
+            placeholder="이름을 입력해주세요."
+            {...register("name")}
+          />
+          <FormErrorMessage>
+            {errors.name && "이름은 최소 2자 이상이어야 합니다."}
+          </FormErrorMessage>
         </FormControl>
         <HStack>
           <Box flexGrow="1">
-            <FormControl id="gender">
+            <FormControl id="gender" isInvalid={Boolean(errors.gender)}>
               <FormLabel>성별</FormLabel>
-              <Select>
-                <option value="option1">남성</option>
-                <option value="option2">여성</option>
+              <Select {...register("gender")}>
+                <option value="man">남성</option>
+                <option value="female">여성</option>
               </Select>
+              <FormErrorMessage>
+                {errors.gender && "성별은 남성/여성 중 하나여야 합니다."}
+              </FormErrorMessage>
             </FormControl>
           </Box>
           <Box flexGrow="1">
-            <FormControl id="birth-date">
+            <FormControl id="birthDate" isInvalid={Boolean(errors.birthDate)}>
               <FormLabel>생년월일</FormLabel>
-              <Input type="date" />
+              <Controller
+                control={control}
+                name="birthDate"
+                render={({ field: { onChange, value } }) => (
+                  <DatePicker onChange={onChange} selected={value} />
+                )}
+              />
+              <FormErrorMessage>
+                {errors.birthDate && "날짜는 yyyy-mm-dd 형식이어야 합니다."}
+              </FormErrorMessage>
             </FormControl>
           </Box>
         </HStack>
-        <FormControl id="nickname">
+        <FormControl id="nickname" isInvalid={Boolean(errors.nickname)}>
           <FormLabel>닉네임</FormLabel>
-          <Input type="text" placeholder="닉네임을 입력해주세요." />
+          <Input
+            type="text"
+            placeholder="닉네임을 입력해주세요."
+            {...register("nickname")}
+          />
+          <FormErrorMessage>
+            {errors.nickname && "닉네임은 최소 2자 이상이어야 합니다."}
+          </FormErrorMessage>
         </FormControl>
-        <FormControl id="email">
+        <FormControl id="email" isInvalid={Boolean(errors.email)}>
           <FormLabel>이메일</FormLabel>
-          <Input type="email" placeholder="이메일을 입력해주세요." />
+          <Input
+            type="email"
+            placeholder="이메일을 입력해주세요."
+            {...register("email")}
+          />
+          <FormErrorMessage>
+            {errors.email?.type === "email"
+              ? "유효한 이메일 형식을 입력하세요."
+              : "이메일은 필수 입력입니다."}
+          </FormErrorMessage>
         </FormControl>
-        <FormControl id="password">
+        <FormControl id="password" isInvalid={Boolean(errors.password)}>
           <FormLabel>비밀번호</FormLabel>
-          <Input type="password" placeholder="비밀번호를 입력해주세요." />
+          <Input
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            {...register("password")}
+          />
+          <FormErrorMessage>
+            {errors.password && "비밀번호는 최소 3자 이상이어야 합니다."}
+          </FormErrorMessage>
         </FormControl>
-        <FormControl id="password-confirm">
+        <FormControl
+          id="confirmPassword"
+          isInvalid={Boolean(errors.confirmPassword)}
+        >
           <FormLabel>비밀번호 확인</FormLabel>
           <Input
             type="password"
             placeholder="비밀번호를 한번 더 입력해주세요."
+            {...register("confirmPassword")}
           />
+          <FormErrorMessage>
+            {errors.confirmPassword?.type === "required"
+              ? "비밀번호 확인은 필수 입력입니다."
+              : "입력된 비밀번호와 일치하지 않습니다."}
+          </FormErrorMessage>
         </FormControl>
         <Box>
           <Text fontSize="sm">이미 계정이 있으신가요?</Text>
@@ -63,7 +175,14 @@ export const RegisterForm = () => {
             로그인
           </ChakraLink>
         </Box>
-        <Button type="submit" colorScheme="cyan" size="lg" isFullWidth my={4}>
+        <Button
+          type="submit"
+          colorScheme="cyan"
+          size="lg"
+          isFullWidth
+          my={4}
+          isLoading={isSubmitting}
+        >
           회원가입
         </Button>
       </Stack>
