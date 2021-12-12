bdb152f4c8b07712be46fbf74fce91205ca6893f
kimin3004 <kimin3004@gmail.com>
2021-12-12 21:49:36 +0900

    feat: ListWithVerticalLine 컴포넌트 추가

---
+++ src/features/schedules/components/ListWithVerticalLine/ListItem.tsx
@@ -0,0 +1,47 @@
+import {
+  Stack,
+  Flex,
+  Circle,
+  Text,
+  useColorModeValue,
+  Heading,
+  StackProps,
+} from "@chakra-ui/react";
+
+export interface ListItemProps extends StackProps {
+  title: string;
+  address: string;
+  phone?: string;
+  isLastItem?: boolean;
+}
+
+export const ListItem = (props: ListItemProps) => {
+  const { title, address, phone, isLastItem, children, ...stackProps } = props;
+
+  return (
+    <Stack as="li" direction="row" spacing="4" {...stackProps}>
+      <Flex direction="column" alignItems="center" aria-hidden="true">
+        <Circle
+          bg={useColorModeValue("cyan.500", "cyan.300")}
+          size="4"
+          mt="2"
+        />
+        {!isLastItem && <Flex flex="1" borderRightWidth="2px" mb="-10" />}
+      </Flex>
+      <Stack spacing="2" pt="1" flex="1">
+        <Heading fontSize="md" fontWeight="semibold">
+          {title}
+        </Heading>
+        <Flex direction="column">
+          <Text fontSize="sm" color={useColorModeValue("gray.600", "gray.400")}>
+            {address}
+          </Text>
+          <Text fontSize="sm" color={useColorModeValue("cyan.600", "cyan.400")}>
+            {phone}
+          </Text>
+        </Flex>
+        <Flex>{children}</Flex>
+      </Stack>
+    </Stack>
+  );
+};
