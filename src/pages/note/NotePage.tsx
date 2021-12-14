a40856857350e073d2babf382952e512d3cca4b6
JEEYOUNG <80609368+JeeyoungPark@users.noreply.github.com>
2021-12-14 16:42:56 +0900

    fix: LinkOverLay를 Link 컴포넌트로 수정 (#31)

--- src/pages/note/NotePage.tsx
+++ src/pages/note/NotePage.tsx
@@ -1,6 +1,8 @@
 import { Heading } from "@chakra-ui/react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 
+import { GoToBackButton } from "@/components/GoToBackButton";
 import { PrivatePageLayout } from "@/components/Layout";
 import { NoteTabs } from "@/features/note/components";
 
@@ -10,7 +12,12 @@ export const NotePage = () => {
   return (
     <PrivatePageLayout
       title="메모 및 투표"
-      header={<Heading size="lg">메모 및 투표</Heading>}
+      header={
+        <>
+          <GoToBackButton />
+          <Heading size="lg">메모 및 투표</Heading>
+        </>
+      }
     >
       <NoteTabs />
     </PrivatePageLayout>
