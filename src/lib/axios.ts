2c25035d7a727ac6f33daaf1860e2c295940f07d
김성백 <dumbuk12@naver.com>
2021-12-15 22:58:49 +0900

    feat: axios interceptor 설정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,33 @@
 import Axios, { AxiosRequestConfig } from "axios";
 
 import { API_URL } from "@/config";
+import { history } from "@/routes";
+import { storage } from "@/utils/storage";
 
-const config: AxiosRequestConfig = { baseURL: API_URL };
+export const axios = Axios.create({
+  baseURL: API_URL,
+  headers: {
+    "content-type": "application/json",
+  },
+});
 
-export const axios = Axios.create(config);
+axios.interceptors.request.use((config: AxiosRequestConfig) => {
+  const token = storage.getToken();
+  if (token) {
+    config.headers = {
+      ...config.headers,
+      Authorization: `${token}`,
+    };
+  }
+  return config;
+});
+
+axios.interceptors.response.use(undefined, (error) => {
+  if (Axios.isAxiosError(error)) {
+    if (error.response?.status === 401) {
+      storage.clearToken();
+      history.replace("/login");
+    }
+  }
+  return Promise.reject(error);
+});
