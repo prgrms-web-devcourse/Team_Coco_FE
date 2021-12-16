7e47b4c4b87169030f7cf8077fd5873dfffaf5e8
김성백 <dumbuk12@naver.com>
2021-12-16 18:15:00 +0900

    feat: 배포 관련 설정 추가 (#36)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,1 @@
-export const API_URL =
-  process.env.NODE_ENV === "development"
-    ? ""
-    : "http://teruteru.ddns.net:39999";
+export const API_URL = process.env.NODE_ENV === "development" ? "" : "/api";
